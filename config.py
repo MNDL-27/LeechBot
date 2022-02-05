8bd0566ed9d107b2a77a65197878ec0c1b6d6463
MNDL-27 <96876555+MNDL-27@users.noreply.github.com>
2022-02-05 23:28:37 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+    TG_BOT_TOKEN = "5258250517:AAHS2nuNH4Vcbo7Dzz4tWaLvBn5OQyqyCbY"
+    # These example values won't work. You must get your own app_id and
+    # api_hash from https://my.telegram.org, under API Development.
+    APP_ID = 1416788
+    API_HASH = "9f878df8e893378cbebd15ccec9f8fb9"
+    # please read https://t.me/c/1279877202/74
+    # if you have not read the above README
+    AUTH_CHANNEL = [
+        -562956193
+    ]
